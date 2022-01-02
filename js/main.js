d19c799fe479b6258857cf1dec03a9aad2b91ee0
marlenakub <marlena.kubicka@wp.pl>
2022-01-02 10:20:46 +0100

    29-js-suma-zmiennych-skonczone

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let firstNumber = 100;
+let secondNumber = 200;
+
+function amount(firstNum, seconNum) 
+{
+    let result = firstNum + secondNumber;
+    return result;
+}
+
+console.log(amount(100, 200));
+
